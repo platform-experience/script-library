1dd3f7096f269a5ce35dbdeed0b579886fb62089
Steven Gregory <sgregory@gmail.com>
2018-12-07 12:27:12 -0600

    Updated category mapping

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,20 +30,44 @@ main() {
 map_category() {
   for i in "${args[0]}"; do
     case $i in
+    body-html-template)
+      replace_content "${CAT_TEMP}" "${CAT_BODY_HTML_TEMPLATE}" ${1}/config.json
+      ;;
     business-rule)
       replace_content "${CAT_TEMP}" "${CAT_BUSINESS_RULE}" ${1}/config.json
+      ;;
+    client-controller)
+      replace_content "${CAT_TEMP}" "${CAT_CLIENT_CONTROLLER}" ${1}/config.json
       ;;
     client-script)
       replace_content "${CAT_TEMP}" "${CAT_CLIENT_SCRIPT}" ${1}/config.json
       ;;
+    css)
+      replace_content "${CAT_TEMP}" "${CAT_CSS}" ${1}/config.json
+      ;;
+    general)
+      replace_content "${CAT_TEMP}" "${CAT_GENERAL}" ${1}/config.json
+      ;;
+    link)
+      replace_content "${CAT_TEMP}" "${CAT_LINK}" ${1}/config.json
+      ;;
+    script-action)
+      replace_content "${CAT_TEMP}" "${CAT_SCRIPT_ACTION}" ${1}/config.json
+      ;;
     script-include)
       replace_content "${CAT_TEMP}" "${CAT_SCRIPT_INCLUDE}" ${1}/config.json
+      ;;
+    server-script)
+      replace_content "${CAT_TEMP}" "${CAT_SERVER_SCRIPT}" ${1}/config.json
       ;;
     ui-action)
       replace_content "${CAT_TEMP}" "${CAT_UI_ACTION}" ${1}/config.json
       ;;
     ui-policy)
       replace_content "${CAT_TEMP}" "${CAT_UI_POLICY}" ${1}/config.json
+      ;;
+    ui-script)
+      replace_content "${CAT_TEMP}" "${CAT_UI_SCRIPT}" ${1}/config.json
       ;;
     workflow)
       replace_content "${CAT_TEMP}" "${CAT_WORKFLOW}" ${1}/config.json
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # categories
+CAT_BODY_HTML_TEMPLATE='Body HTML Template'
 CAT_BUSINESS_RULE='Business Rule'
+CAT_CLIENT_CONTROLLER='Client Controller'
 CAT_CLIENT_SCRIPT='Client Script'
+CAT_CSS='CSS'
+CAT_GENERAL='General'
+CAT_LINK='Link'
+CAT_SCRIPT_ACTION='Script Action'
 CAT_SCRIPT_INCLUDE='Script Include'
+CAT_SERVER_SCRIPT='Server Script'
 CAT_UI_ACTION='UI Action'
 CAT_UI_POLICY='UI Policy'
+CAT_UI_SCRIPT='UI Script'
 CAT_WORKFLOW='Workflow'
 
 # gists
