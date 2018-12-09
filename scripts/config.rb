2a14502dd2693e1bc78c4378a7fba4d57208ebdb
Steven Gregory <sgregory@gmail.com>
2018-12-09 16:36:57 -0600

    Crafted ruby scaffold solution

---
+++ scripts/config.rb
@@ -0,0 +1,10 @@
+module Config
+  CONFIG_GIST = 'https://gist.githubusercontent.com/stevengregory/27b8623c2fc4456544f63ce3b3b70edd/raw/e6ec77722383da95398ba3d7019808a7e3fc2e1d/config.json'
+  README_GIST = 'https://gist.githubusercontent.com/stevengregory/312026b03c90b695c9052e54eaa1cc87/raw/e79500c1a736997806897149a19371f5d4eaa2db/README.md'
+  CAT_TEMP = 'Temp Category'
+  DETAILS_TEMP = 'Temp details'
+  NAME_TEMP = 'temp-name'
+  COMMON_NAME_TEMP = 'Temp Common Name'
+  SOLUTION_DETAILS_TEMP = 'Temp details'
+  TITLE_TEMP = 'Temp Title'
+end
---
+++ scripts/scaffold.rb
@@ -0,0 +1,51 @@
+require 'fileutils'
+require 'nokogiri'
+require_relative 'config'
+
+def replace_content(arg1, arg2, arg3)
+  %x( sed -i '' -e "s/#{arg1}/#{arg2}/g" #{arg3} )
+end
+
+def create_config(dir_name, category, details)
+  config_dir = "#{dir_name}/config.json"
+  %x( curl #{Config::CONFIG_GIST} > #{config_dir} )
+  replace_content(Config::CAT_TEMP, category, config_dir)
+  replace_content(Config::SOLUTION_DETAILS_TEMP, details, config_dir)
+end
+
+def create_readme(dir_name, description, details)
+  readme_dir = "#{dir_name}/README.md"
+  %x( curl #{Config::README_GIST} > #{readme_dir} )
+  replace_content(Config::TITLE_TEMP, description, readme_dir)
+  replace_content(Config::SOLUTION_DETAILS_TEMP, details, readme_dir)
+end
+
+def to_slug(category)
+  return category.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+end
+
+doc = Nokogiri::XML(File.open("tmp/scripts.xml"))
+persons = doc.xpath("//x_snc_servicenow_s_servicenow_scripting")
+persons_data = persons.map {|person|
+  {
+    type: person.xpath("./script_type").text,
+    description: person.xpath("./short_description").text,
+    details: person.xpath("./details").text,
+    script: person.xpath("./script").text
+  }
+}
+
+count = 1
+persons_data.each do |item|
+  if !item[:script].empty? && !item[:type].empty? && !item[:description].empty?
+    category = item[:type]
+    description = item[:description]
+    details = !item[:details].empty? ? item[:details] : item[:description]
+    dir_name = "dist/#{to_slug(category)}/si-script-#{count}"
+    FileUtils.mkdir_p(dir_name) unless File.exists?(dir_name)
+    File.write("#{dir_name}/script.js", item[:script])
+    create_config(dir_name, category, description)
+    create_readme(dir_name, description, details)
+    count = count + 1
+  end
+end
