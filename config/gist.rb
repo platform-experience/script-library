e41be3166e14b474ed8105f026475fe3af14d835
Steven Gregory <sgregory@gmail.com>
2018-12-19 18:03:15 -0600

    Config & gist updates

--- config/gist.rb
+++ config/gist.rb
@@ -1,4 +1,4 @@
 module Gist
-  CONFIG = 'https://gist.githubusercontent.com/stevengregory/27b8623c2fc4456544f63ce3b3b70edd/raw/e6ec77722383da95398ba3d7019808a7e3fc2e1d/config.json'
+  CONFIG = 'https://gist.githubusercontent.com/stevengregory/8678d518182fd6f3497680ae1e26e870/raw/7637dbb7afa8915daf3527b5438b2aadad3d1cfd/config.json'
   README = 'https://gist.githubusercontent.com/stevengregory/312026b03c90b695c9052e54eaa1cc87/raw/e79500c1a736997806897149a19371f5d4eaa2db/README.md'
 end
