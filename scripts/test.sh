14ea3c486eedce1e55259c8d3cf76b4b8840e32f
Steven Gregory <sgregory@gmail.com>
2018-12-21 13:13:55 -0600

    Updated sample generation script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,8 @@
 
 source ./config/scaffold.sh
 
-yarn build client-controller script-115 "Embedded Widget" "A Client Controller script using spUtil.get() to embed a widget in Service Portal."
-curl ${CTRL_GIST} > src/client-controller/si-script-115/script.js
+script_name=script-115
+yarn build client-controller $script_name "Embedded Widget" "A Client Controller script using spUtil.get() to embed a widget in Service Portal."
+curl ${CTRL_GIST} > src/client-controller/si-$script_name/script.js
 git add .
 git commit -a -m "Added client controller sample script"
