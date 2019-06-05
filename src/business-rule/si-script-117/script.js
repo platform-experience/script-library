5b884fd8e2e46128af9361cd4f5e68a5bcc34a94
Steven Gregory <sgregory@gmail.com>
2019-06-05 10:43:31 -0500

    Prettier goodness

--- src/business-rule/si-script-117/script.js
+++ src/business-rule/si-script-117/script.js
@@ -1,45 +1,44 @@
 (function executeRule(current, previous /*null when async*/) {
+  var wo = new GlideRecord('wm_order');
+  var totalTime = 0;
 
-	var wo = new GlideRecord('wm_order');
-	var totalTime = 0;
+  wo.addQuery('initiated_from', current.sys_id);
+  wo.query();
 
-	wo.addQuery('initiated_from', current.sys_id);
-	wo.query();
+  while (wo.next()) {
+    var wot = new GlideRecord('wm_task');
+    wot.addQuery('parent', wo.getUniqueValue());
+    wot.query();
+    while (wot.next()) {
+      var tw = new GlideRecord('task_time_worked');
+      tw.get('task', wot.getUniqueValue());
 
-	while (wo.next()) {
-		var wot = new GlideRecord('wm_task');
-		wot.addQuery('parent', wo.getUniqueValue());
-		wot.query();
-		while (wot.next()) {
+      var gdTime = new GlideDuration();
+      gdTime.setValue(tw.getValue('time_worked'));
+      totalTime += gdTime.getNumericValue();
+    }
+  }
 
-			var tw = new GlideRecord('task_time_worked');
-			tw.get('task', wot.getUniqueValue());
+  // case time worked
+  var gdCaseTime = new GlideDuration();
+  gdCaseTime.setValue(current.getValue('time_worked'));
+  totalTime += gdCaseTime.getNumericValue();
 
-			var gdTime = new GlideDuration();
-			gdTime.setValue(tw.getValue('time_worked'));
-			totalTime += gdTime.getNumericValue();
-		}
-	}
+  var gdTest = new GlideDuration(totalTime);
+  gs.info('totalTime: ' + gdTest.getDisplayValue());
+  totalTime = Math.round(totalTime / 1000 / 60 / 60);
 
-	// case time worked
-	var gdCaseTime = new GlideDuration();
-	gdCaseTime.setValue(current.getValue('time_worked'));
-	totalTime += gdCaseTime.getNumericValue();
+  gs.info('Final result: ' + totalTime + ' hours');
 
-	var gdTest = new GlideDuration(totalTime);
-	gs.info('totalTime: ' + gdTest.getDisplayValue());
-	totalTime = Math.round(totalTime / 1000 / 60 / 60);
-	
-	gs.info('Final result: ' + totalTime + ' hours');
-	
-	
-	var entitlement = new GlideRecord("service_entitlement");
-	if(entitlement.get(current.entitlement) && entitlement.getValue("unit") == "hours"){
-		var rem_hours = entitlement.getValue("remaining_units");
-		if(rem_hours > 0){
-			entitlement.setValue("remaining_units", rem_hours - totalTime);
-			entitlement.update();
-		}
-	}
-
+  var entitlement = new GlideRecord('service_entitlement');
+  if (
+    entitlement.get(current.entitlement) &&
+    entitlement.getValue('unit') == 'hours'
+  ) {
+    var rem_hours = entitlement.getValue('remaining_units');
+    if (rem_hours > 0) {
+      entitlement.setValue('remaining_units', rem_hours - totalTime);
+      entitlement.update();
+    }
+  }
 })(current, previous);
