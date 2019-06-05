5e24859b16c76174a235b44c90dadffe036f7d2b
Allisa Liberto <33538529+All-isa@users.noreply.github.com>
2019-06-05 09:11:43 -0700

    Entitlement update business rule (#1)

---
+++ src/business-rule/si-script-117/README.md
@@ -0,0 +1,5 @@
+# Update entitlement hours after workorders
+
+## Description
+
+Capture the hours worked on workorders from cases, add them together for a total time, and deduct from entitlement.
---
+++ src/business-rule/si-script-117/script.js
@@ -0,0 +1,41 @@
+(function executeRule(current, previous /*null when async*/) {
+  var wo = new GlideRecord('wm_order');
+  var totalTime = 0;
+
+  wo.addQuery('initiated_from', current.sys_id);
+  wo.query();
+
+  while (wo.next()) {
+    var wot = new GlideRecord('wm_task');
+    wot.addQuery('parent', wo.getUniqueValue());
+    wot.query();
+    while (wot.next()) {
+      var tw = new GlideRecord('task_time_worked');
+      tw.get('task', wot.getUniqueValue());
+
+      var gdTime = new GlideDuration();
+      gdTime.setValue(tw.getValue('time_worked'));
+      totalTime += gdTime.getNumericValue();
+    }
+  }
+
+  // case time worked
+  var gdCaseTime = new GlideDuration();
+  gdCaseTime.setValue(current.getValue('time_worked'));
+  totalTime += gdCaseTime.getNumericValue();
+
+  var gdTest = new GlideDuration(totalTime);
+  totalTime = Math.round(totalTime / 1000 / 60 / 60);
+
+  var entitlement = new GlideRecord('service_entitlement');
+  if (
+    entitlement.get(current.entitlement) &&
+    entitlement.getValue('unit') == 'hours'
+  ) {
+    var rem_hours = entitlement.getValue('remaining_units');
+    if (rem_hours > 0) {
+      entitlement.setValue('remaining_units', rem_hours - totalTime);
+      entitlement.update();
+    }
+  }
+})(current, previous);
